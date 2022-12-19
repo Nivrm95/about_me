5d06ea96222113b4bda853724d495232f787901a
Niv Rahamim <nivrm@gmail.com>
2022-12-19 18:18:53 +0200

    function button js

---
+++ main.js
@@ -0,0 +1,10 @@
+function handleClick() {
+console.log ("Element Clicked");
+PlacesIIove.style.color="red";
+ContactMe.style.color="red";
+}
+
+
+function delayHandleClick(){
+setTimeout(handleClick, 3000);
+}
