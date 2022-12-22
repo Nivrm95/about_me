a73ea805cf60944006028d1953e597855d205c89
Niv Rahamim <nivrm@gmail.com>
2022-12-22 09:47:50 +0200

    add button dark mood

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-function handleClick() {
-console.log ("Element Clicked");
-PlacesIIove.style.color="red";
-ContactMe.style.color="red";
+function handleClick(id) {
+    console.log ("Element Clicked");
+    document.getElementById(id).style.color="red";
+//PlacesIIove.style.color="red";
+//ContactMe.style.color="red";
 }
 
 
-function delayHandleClick(){
-setTimeout(handleClick, 3000);
+function delayHandleClick(id) {
+  setTimeout(()=>handleClick(id), 3000);
+}
+
+function dark(){
+let flag = document.getElementById(`Dark`).value;
+if (flag == 'off'){
+  document.body.setAttribute('class', 'darkMood')
+  document.getElementById('Dark').value = 'on'
+
+} else{ 
+  document.body.setAttribute('class', 'body')
+  document.getElementById('Dark').value = 'off' 
+}
 }
